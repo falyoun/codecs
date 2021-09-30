14522eecec179cc295f2877a51fd7dbcfb5b8fee
AbdulrahmanFalyounST <abd.falyoun@scandinavia.com>
2021-09-30 16:29:06 +0300

    [FIX]: enum type

--- src/codecs/models/common/io-element.ts
+++ src/codecs/models/common/io-element.ts
@@ -1,8 +1,10 @@
+import { PacketTypesEnum } from '@app/codecs';
+
 export class IoElement {
   id: number;
   value: number;
   label: string;
   dimension: string;
   valueHuman: string;
-  type: string;
+  type: PacketTypesEnum;
 }
