eafe52c860c028d6ef4efa27b438df3db10b1c7c
Rami-K-Youssef <79768418+Rami-K-Youssef@users.noreply.github.com>
2021-11-03 16:16:44 +0200

    updates (#2)
    Co-authored-by: Rami Kass Youssef <rami.k.youssef@scandinaviatech.com>

--- src/codecs/codecs-for-communication-over-gprs-messages/cogm-base-class.ts
+++ src/codecs/codecs-for-communication-over-gprs-messages/cogm-base-class.ts
@@ -1,12 +1,5 @@
 import { Command, TcpCFCOGMPacketBody } from '@app/codecs';
-import { BinaryReader, BinaryWriter } from 'buffer-sdk';
-
 export abstract class CogmBaseClass {
-  protected constructor(
-    public reader: BinaryReader,
-    public writer: BinaryWriter,
-  ) {}
-
-  public abstract decode(): TcpCFCOGMPacketBody;
+  public abstract decode(data: Buffer): TcpCFCOGMPacketBody;
   public abstract encode(command: Command): Buffer;
 }
